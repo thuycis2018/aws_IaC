8780f16c52f31fa9d439ad60e345f7d7d614c7af
TL <thuycis@gmail.com>
2024-08-05 21:55:01 -0700

    feat(orders): set up serverless config to use kinesis and lambda

---
+++ handler.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const orderManager = require('./orderManager');
+const kinesisHelper = require('./kinesisHelper');
+const producerManager = require('./producerManager');
+const deliveryManager = require('./deliveryManager');
+
+function createResponse(statusCode, message) {
+  const response = {
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  };
+
+  return response;
+}
+
+module.exports.createOrder = async (event) => {
+
+  const body = JSON.parse(event.body);
+  const order = orderManager.createOrder(body);
+
+  return orderManager.placeNewOrder(order).then(() => {
+    return createResponse(200, order);
+  }).catch(error => {
+    return createResponse(400, error);
+  })
+};
+
+module.exports.orderFulfillment = async (event) => {
+  const body = JSON.parse(event.body);
+  const orderId = body.orderId;
+  const fulfillmentId = body.fulfillmentId;
+
+  return orderManager.fulfillOrder(orderId, fulfillmentId).then(() => {
+    return createResponse(200, `Order with orderId:${orderId} was sent to delivery`);
+  }).catch(error => {
+    return createResponse(400, error);
+  })
+}
+
+module.exports.notifyExternalParties = async (event) => {
+  const records = kinesisHelper.getRecords(event);
+
+  const producerPromise = getProducerPromise(records);
+  const deliveryPromise = getDeliveryPromise(records);
+
+  return Promise.all([producerPromise, deliveryPromise]).then(() => {
+    return 'everything went well'
+  }).catch(error => {
+    return error;
+  })
+}
+
+module.exports.notifyDeliveryCompany = async (event) => {
+  // Some HTTP call!
+  console.log('Lets imagine that we call the delivery company endpoint')
+
+  return 'done';
+}
+
+module.exports.orderDelivered = async (event) => {
+  const body = JSON.parse(event.body);
+  const orderId = body.orderId;
+  const deliveryCompanyId = body.deliveryCompanyId;
+  const orderReview = body.orderReview;
+
+  return deliveryManager.orderDelivered(orderId, deliveryCompanyId, orderReview).then(( ) => {
+    return createResponse(200, `Order with ${orderId} was delivered successfully by companyId ${deliveryCompanyId})`);
+  }).catch(error => {
+    return createResponse(400, error);
+  })
+}
+
+module.exports.notifyCustomerService = async (event) => {
+  console.log('Lets imagine that we call the customer service endpoint');
+
+  return 'done';
+}
+
+function getProducerPromise(records) {
+  const ordersPlaced = records.filter(r => r.eventType === 'order_placed');
+
+  if (ordersPlaced.length > 0) {
+    return producerManager.handlePlacedOrders(ordersPlaced);
+  } else {
+    return null;
+  }
+}
+
+function getDeliveryPromise(records) {
+  const orderFulfilled = records.filter(r => r.eventType === 'order_fulfilled');
+
+  if (orderFulfilled.length > 0) {
+    return deliveryManager.deliveryOrder(orderFulfilled);
+  } else {
+    return null;
+  }
+}
